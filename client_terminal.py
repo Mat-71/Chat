842053c72d3bf8e5c2abc5636cc2851a07fa4afe
celidur <gaetansjo@gmail.Com>
2022-07-24 20:53:00 -0400

    update message

--- client_terminal.py
+++ client_terminal.py
@@ -1,3 +1,5 @@
+import datetime
+
 from encryption import *
 import socket
 import sys
@@ -5,76 +7,127 @@ import errno
 from key_generator import get_key_from_password
 
 sys.setrecursionlimit(15000)
-HEADER_LENGTH = 10
-IP = "localhost"
-PORT = 42690
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((IP, PORT))
-client_socket.setblocking(False)
 
 
-def receive():
-    while True:
-        try:
-            header = client_socket.recv(HEADER_LENGTH)
-            if not len(header):
-                print('Connection closed by the server')
+class Client:
+    def __init__(self, _username, _key, new=True):
+        self.IP = "localhost"
+        self.PORT = 42690
+        self.HEADER_LENGTH = 10
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket.connect((self.IP, self.PORT))
+        self.client_socket.setblocking(False)
+        self.username = _username
+        self.key = _key
+        self._send("key")
+        self.s_key = self.receive()
+        self.s_key = self.s_key.split(",")
+        self.s_key = (int(self.s_key[0]), int(self.s_key[1]))
+        if new:
+            self.state = self.new_login()
+        else:
+            self.state = self.login()
+
+    def get_state(self):
+        return self.state
+
+    def receive(self):
+        while True:
+            try:
+                header = self.client_socket.recv(self.HEADER_LENGTH)
+                if not len(header):
+                    print('Connection closed by the server')
+                    sys.exit()
+                header = int(header.decode('utf-8').strip())
+                m = self.client_socket.recv(header).decode('utf-8')
+                return m
+            except IOError as e:
+                if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
+                    print('Reading error: {}'.format(str(e)))
+                    sys.exit()
+            except Exception as e:
+                print('Reading error: '.format(str(e)))
                 sys.exit()
-            header = int(header.decode('utf-8').strip())
-            m = client_socket.recv(header).decode('utf-8')
-            return m
-        except IOError as e:
-            if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
-                print('Reading error: {}'.format(str(e)))
-                sys.exit()
-        except Exception as e:
-            print('Reading error: '.format(str(e)))
-            sys.exit()
 
+    def _send(self, _message):
+        header = f"{len(_message.encode('utf-8')):<{self.HEADER_LENGTH}}".encode('utf-8')
+        self.client_socket.send(header + _message.encode('utf-8'))
 
-def send(message):
-    header = f"{len(message.encode('utf-8')):<{HEADER_LENGTH}}".encode('utf-8')
-    client_socket.send(header + message.encode('utf-8'))
-
-
-def login(user: str):
-    user = encrypt('login|' + user, s_key)
-    send(user)
-    check = receive()
-    if check == "-1":
-        return -1
-    check = encrypt('check|' + decrypt(check, key[1]), s_key)
-    send(check)
-    if receive() == "0":
-        return 0
-    return 1
-
-
-def new_login(user: str):
-    send(encrypt('newlogin|' + str(key[0][0]) + "," + str(key[0][1]) + "|" + encrypt(user, key[1]), s_key))
-    a = receive()
-    if a == '-1':
+    def login(self):
+        user = encrypt('login|' + self.username, self.s_key)
+        self._send(user)
+        check = self.receive()
+        if check == "-1":
+            return -1
+        check = encrypt('check|' + decrypt(check, self.key[1]), self.s_key)
+        self._send(check)
+        if self.receive() == "0":
+            return 0
         return 1
-    a = decrypt(a, s_key)
-    if a == user:
-        send(encrypt("check2|0", s_key))
-        return 0
-    send(encrypt("check2|-1", s_key))
-    return -1
+
+    def new_login(self):
+        self._send(encrypt(
+            'newlogin|' + str(self.key[0][0]) + "," + str(self.key[0][1]) + "|" + encrypt(self.username, self.key[1]),
+            self.s_key))
+        a = self.receive()
+        if a == '-1':
+            return 1
+        a = decrypt(a, self.s_key)
+        if a == self.username:
+            self._send(encrypt("check2|0", self.s_key))
+            return 0
+        self._send(encrypt("check2|-1", self.s_key))
+        return -1
+
+    def get_friend(self):
+        self._send(encrypt("getfriend|", self.s_key))
+        a = decrypt(self.receive(), self.key[1])
+        return a.split("|") if a != "" else []
+
+    def get_pending(self):
+        self._send(encrypt("getpending|", self.s_key))
+        a = decrypt(self.receive(), self.key[1])
+        return a.split("|") if a != "" else []
+
+    def get_request(self):
+        self._send(encrypt("getrequest|", self.s_key))
+        a = decrypt(self.receive(), self.key[1])
+        return a.split("|") if a != "" else []
+
+    def friend_request(self, _friend):
+        self._send(encrypt("friendrequest|" + _friend, self.s_key))
+        return self.receive()
+
+    def friend_accept(self, _friend):
+        self._send(encrypt("friendaccept|" + _friend, self.s_key))
+        return self.receive()
+
+    def send_message(self, _friend, _message):
+        self._send(encrypt("message|" + _friend + "|" + encrypt(_message, self.get_friend_key(_friend)), self.s_key))
+        a = self.receive()
+        if a == "-1":
+            return -1
+        if a == "1":
+            return 1
+        return datetime.datetime.strptime(a, '%Y-%m-%d %H:%M:%S')
+
+    def get_friend_key(self, _friend):
+        self._send(encrypt("getfriendkey|" + _friend, self.s_key))
+        a = decrypt(self.receive(), self.key[1]).split("|")
+        return int(a[0]), int(a[1])
 
 
-send("key")
-s_key = receive()
-s_key = s_key.split(",")
-s_key = (int(s_key[0]), int(s_key[1]))
-
-username = input()
-password = input()
+username = "admin"
+password = ""
 key = get_key_from_password(username + password)
+client = Client(username, key, new=False)
 
-# new_login(username) -> 1:username pris , 0:compte correctement créé, -1: erreur envoie serveur
-# login(username) -> 1:mauvais mdp pour ce compte, 0:compte correctement connecté, -1: identifiant inconnu
+# new_login() -> 1:username pris , 0:compte correctement créé, -1: erreur envoie serveur
+# login() -> 1:mauvais mdp pour ce compte, 0:compte correctement connecté, -1: identifiant inconnu
 
 if __name__ == "__main__":
-    print(new_login(username))
-    print(login(username))
+    print(client.get_state())
+    print(client.get_friend())
+    print(client.get_pending())
+    print(client.get_request())
+    print(client.send_message("a", "salut"))
--- serveur.py
+++ serveur.py
@@ -1,4 +1,6 @@
+import datetime
 import socket
+
 import select
 from key_generator import get_key_from_password
 from encryption import *
@@ -20,39 +22,75 @@ key = get_key_from_password('5', 8)
 print(f'Listening for connections on {IP}: {PORT}...')
 
 
-class user:
-    def __init__(self, username, friend=None, request=None, pending=None):
+class User:
+    def __init__(self, _username, _key, _id=0, _message=None, friend=None, request=None, pending=None):
         if pending is None:
             pending = []
         if request is None:
             request = []
         if friend is None:
             friend = []
-        self.username = username
+        if _message is None:
+            _message = []
+        self.message = _message
+        self.username = _username
+        self.key = _key
         self.friend = friend
         self.request = request
         self.pending = pending
+        self.id = _id
 
     def get_username(self):
         return self.username
 
+    def get_key(self):
+        return self.key
+
     def get_friend(self):
-        return self.friend
+        return "|".join(self.friend)
 
     def get_request(self):
-        return self.request
+        return "|".join(self.request)
 
     def get_pending(self):
-        return self.pending
+        return "|".join(self.pending)
 
     def add_friend(self, friend):
         self.friend.append(friend)
+        if friend in self.request:
+            self.request.remove(friend)
+        if friend in self.pending:
+            self.pending.remove(friend)
 
     def add_request(self, request):
         self.request.append(request)
 
     def add_pending(self, pending):
         self.pending.append(pending)
+
+    def new_message(self, _message, _username):
+        self.message.append(Message(self.id, _username, _message))
+        self.id += 1
+
+    def get_message(self):
+        return self.message
+
+
+class Message:
+    def __init__(self, _id, _username: str, _message: str, date=datetime.datetime.now().replace(microsecond=0)):
+        self.username = _username
+        self.message = _message
+        self.date = date
+        self.id = _id
+
+    def __str__(self):
+        return f'{len(self.message)}|{self.id}|{self.date}|{self.username}|{self.message}'
+
+    def __repr__(self):
+        return f'{len(self.message)}|{self.id}|{self.date}|{self.username}|{self.message}'
+
+    def get_date(self):
+        return self.date
 
 
 def receive_message(client_socket):
@@ -66,12 +104,13 @@ def receive_message(client_socket):
         return False
 
 
-def send(message, a):
-    message = message.encode('utf-8')
-    header = f"{len(message):<{HEADER_LENGTH}}".encode('utf-8')
-    a.send(header + message)
+def send(_message, a):
+    _message = _message.encode('utf-8')
+    header = f"{len(_message):<{HEADER_LENGTH}}".encode('utf-8')
+    a.send(header + _message)
 
-
+_user['admin'] = User('admin', get_key_from_password('admin')[0], friend=['a'])
+_user['a'] = User('a', get_key_from_password('a')[0], friend=['admin'])
 while True:
     read_sockets, _, exception_sockets = select.select(sockets_list, [], sockets_list)
     for notified_socket in read_sockets:
@@ -88,15 +127,19 @@ while True:
                 send(private, notified_socket)
                 continue
             m = decrypt(m, key[1]).split("|")
+            if notified_socket in client3:
+                user = _user[client3[notified_socket]]
+            else:
+                user = False
             match m[0]:
                 case 'login':
-                    user = m[1]
-                    if user not in _user:
+                    username = m[1]
+                    if username not in _user:
                         send("-1", notified_socket)
                         break
-                    _check = str(random.Random().randint(0, 100000))
-                    client[notified_socket] = [_check, user]
-                    check = encrypt(_check, _user[user][0])
+                    _check = str(random.Random().randint(0, 10000000000000))
+                    client[notified_socket] = [_check, username]
+                    check = encrypt(_check, _user[username].get_key())
                     send(check, notified_socket)
                 case 'newlogin':
                     user_key = m[1].split(',')
@@ -114,7 +157,7 @@ while True:
                 case 'check':
                     if notified_socket not in client:
                         continue
-                    user_key = _user[client[notified_socket][1]][0]
+                    user_key = _user[client[notified_socket][1]].get_key()
                     if client[notified_socket][0] == decrypt(m[1], user_key):
                         send('0', notified_socket)
                     else:
@@ -126,51 +169,70 @@ while True:
                     if notified_socket not in client2:
                         continue
                     if m[1] == "0":
-                        _user[client2[notified_socket][0]] = [client2[notified_socket][1], [], [], []]
+                        _user[client2[notified_socket][0]] = User(client2[notified_socket][0],
+                                                                  client2[notified_socket][1])
                         print('Accepted new connection from {}'.format(client2[notified_socket][0]))
                         client3[notified_socket] = client2[notified_socket][0]
                     del client2[notified_socket]
                 case 'getfriend':
-                    if notified_socket in client3:
-                        send(encrypt(_user[client3[notified_socket]][1], _user[client3[notified_socket]][0]),
-                             notified_socket)
+                    if user:
+                        send(encrypt(user.get_friend(), user.get_key()), notified_socket)
                     else:
                         send("-1", notified_socket)
-                case 'newfriend':
-                    if notified_socket in client3:
-                        if m[1] in _user:
-                            _user[m[1]][3].append(client3[notified_socket])
-                            _user[client3[notified_socket]][2].append(m[1])
+                case 'friendrequest':
+                    if user:
+                        if m[1] in _user and m[1] != user.get_username():
+                            _user[m[1]].add_request(user.get_username())
+                            user.add_pending(m[1])
                             send('0', notified_socket)
                         else:
                             send('1', notified_socket)
                     else:
                         send("-1", notified_socket)
-                case 'getfriendaccept':
-                    if notified_socket in client3:
-                        send(encrypt(_user[client3[notified_socket]][3], _user[client3[notified_socket]][0]),
-                             notified_socket)
+                case 'getrequest':
+                    if user:
+                        send(encrypt(user.get_request(), user.get_key()), notified_socket)
                     else:
                         send("-1", notified_socket)
                 case 'friendaccept':
-                    if notified_socket in client3:
-                        if m[1] in _user[client3[notified_socket]][3]:
-                            _user[client3[notified_socket]][1].append(m[1])
-                            _user[client3[notified_socket]][3].remove(m[1])
-                            _user[m[1]][1].append(client3[notified_socket])
-                            _user[m[1]][2].remove(client3[notified_socket])
+                    if user:
+                        if m[1] in user.get_request()and m[1] != user.get_username():
+                            user.add_friend(m[1])
+                            _user[m[1]].add_friend(user.get_username())
                             send("0", notified_socket)
                         else:
                             send("1", notified_socket)
                     else:
                         send("-1", notified_socket)
-                case 'friendpending':
-                    if notified_socket in client3:
-                        send(encrypt(_user[client3[notified_socket]][2], _user[client3[notified_socket]][0]),
-                             notified_socket)
+                case 'getpending':
+                    if user:
+                        send(encrypt(user.get_pending(), user.get_key()), notified_socket)
                     else:
                         send("-1", notified_socket)
+                case 'getfriendkey':
+                    if user:
+                        if m[1] in user.get_friend():
+                            a = _user[m[1]].get_key()
+                            a = str(a[0]) + "|" + str(a[1])
+                            send(encrypt(a, user.get_key()), notified_socket)
+                        else:
+                            send("1", notified_socket)
+                    else:
+                        send("-1", notified_socket)
+                case 'message':
+                    while len(m) > 3:
+                        m[2] = m[2] + "|" + m[3]
+                        del m[3]
+                    if user:
+                        if m[1] in user.get_friend():
+                            _user[m[1]].new_message(m[2], user.get_username())
+                            date = str(_user[m[1]].get_message()[-1].get_date())
 
+                            send(date, notified_socket)
+                        else:
+                            send("1", notified_socket)
+                    else:
+                        send("-1", notified_socket)
                 case _:
                     print("erreur commande")
     for notified_socket in exception_sockets:
